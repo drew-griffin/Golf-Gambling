1b6b407a82ce97124cbc1075b9c9d34a112e9bbc
Drew Seidel <Dgriffinseidel@gmail.com>
2020-09-14 17:21:53 -0700

    Initial Commit

---
+++ Golf Gambling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golf Gambling
+//
+//  Created by Drew Seidel on 9/14/20.
+//  Copyright © 2020 Drew Seidel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
